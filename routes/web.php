ffdd4aca28f82e0c7530dc520a7626d707993a30
Josh Jones <josh@jtbj.co.uk>
2025-01-20 15:31:41 +0000

    chore: add search routes to config

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,3 +13,4 @@ Route::get('/home', function () {
 })->name('home');
 
 Route::resource('contacts', ContactController::class);
+Route::resource('search', SearchController::class);
